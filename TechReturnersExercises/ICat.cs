946c7f608f00cb266373e7eb45fb0f9abedd930a
Osman Elsahib <elsahib10@gmail.com>
2020-12-02 14:23:25 +0000

    declearing classes

--- TechReturnersExercises/ICat.cs
+++ TechReturnersExercises/ICat.cs
@@ -3,7 +3,38 @@ using System;
 namespace TechReturners.Exercises
 {
     public interface ICat
-    {
+    {   
+        String Eat();
+        Int32 AverageHeight();
+        String Setting();
+        Boolean IsAsleep();
 
     }
+
+    public class DomesticCat : ICat
+    {
+    public static String Eat() {
+        return "Purrrrrrr";
+    }
+       public static Int32 AverageHeight() {
+        return 23;
+    }
+       public static String Setting() {
+        return "domestic";
+    }
+       public static Boolean IsAsleep() {
+        return ;
+    }
+
+}
+    public class LionCat{
+    public static Boolean Eat() {
+
+    }
+}
+    public class CheetahCat{
+    public static Boolean Eat() {
+
+    }
+}
 }
